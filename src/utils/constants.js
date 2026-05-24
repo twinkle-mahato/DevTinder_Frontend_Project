f109ef2b50e15785bc603d74cebaa131543e2dcf
twinkle-mahato <twinklemahato8670@gmail.com>
2026-05-24 07:28:42 +0530

    add backend url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,4 @@
 export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:7777" : "/api";
+  location.hostname === "localhost"
+    ? "http://localhost:7777"
+    : "https://devtinder-backend-55ul.onrender.com";
